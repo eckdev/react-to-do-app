5278e4946775633d68107914b2632b4291567915
eckdev <eyupcankayadarcin@hotmail.com>
2020-09-17 07:57:58 +0300

    insert client build file

---
+++ client/build/precache-manifest.d26ab2a5f09d4f23bf6420f27fd43c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49da0e3b232c170d20e129bae99e30bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "551e4076b4359d755113",
+    "url": "/static/css/main.d40ef1fd.chunk.css"
+  },
+  {
+    "revision": "8e6c2e18097b1c6d42a1",
+    "url": "/static/js/2.e62526b1.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.e62526b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "551e4076b4359d755113",
+    "url": "/static/js/main.924a97a9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
